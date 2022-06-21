7f5f0da79f5c17510327f4db3f37f44e9fbdbedb
Alexander Terentev <terentew@mail.ru>
2022-06-21 10:15:59 +0300

    4

--- src/test/java/cloud/autotests/config/Project.java
+++ src/test/java/cloud/autotests/config/Project.java
@@ -10,7 +10,7 @@ public class Project {
     }
 
     public static boolean isRemoteWebDriver() {
-        return config.remoteDriverUrl().equals("");
+        return !config.remoteDriverUrl().equals("");
     }
 
     public static boolean isVideoOn() {
